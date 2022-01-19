cc8437a64abf16b928808d5fc8befa12db69d7c0
Sabeeh Zaidi <sabeehz@gmail.com>
2022-01-19 22:21:04 +0500

    added root to map

--- app/src/main/java/com/sabeeh/foursquareandroid/MapsFragment.kt
+++ app/src/main/java/com/sabeeh/foursquareandroid/MapsFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import coil.load
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -44,15 +45,6 @@ class MapsFragment : Fragment() {
     private lateinit var mMap : GoogleMap
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         val sydney = LatLng(-34.0, 151.0)
         mMap = googleMap
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
@@ -64,7 +56,8 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -94,7 +87,7 @@ class MapsFragment : Fragment() {
             when (response) {
                 is NetworkResult.Success -> {
                     response.data?.let {
-                        analyticsService.logEvent("Response Successful")
+                        analyticsService.logEvent("Response Successful. ${response.data.results}")
                     }
                 }
 
